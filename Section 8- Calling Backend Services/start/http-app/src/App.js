46ede9fcdb2ea6fafe26ca989d5710d1d9416b4f
loranBepatient <laurent.muller@bepatient.com>
2018-11-19 13:57:07 +0100

    Firt implementations of http requests

--- Section 8- Calling Backend Services/start/http-app/src/App.js
+++ Section 8- Calling Backend Services/start/http-app/src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from "react";
+
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  url = "https://jsonplaceholder.typicode.com/posts";
   state = {
     posts: []
   };
 
-  handleAdd = () => {
-    console.log("Add");
+  async componentDidMount() {
+    const { data: posts } = await axios.get(this.url);
+    this.setState({ posts });
+  }
+
+  handleAdd = async () => {
+    const newPost = {
+      title: "new post title",
+      body: "new post content"
+    };
+
+    const { data: post } = await axios.post(this.url, newPost);
+
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
+
+    console.log(this.state.posts);
   };
 
-  handleUpdate = post => {
-    console.log("Update", post);
+  handleUpdate = async post => {
+    post.title = "updated title";
+    await axios.put(`${this.url}/${post.id}`, post);
+
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(post.id);
+    posts[index] = { ...post };
+    this.setState({ posts });
   };
 
-  handleDelete = post => {
-    console.log("Delete", post);
+  handleDelete = async post => {
+    await axios.delete(`${this.url}/${post.id}`);
+
+    const posts = this.state.posts.filter(_post => post.id !== _post.id);
+    this.setState({ posts });
   };
 
   render() {
